b33f71a30fc7f7c8cf6a572b66f4b1bd9f3e056c
Jarif Ahmed <your_github_email@gmail.com>
2026-02-09 23:12:07 +0600

    added all files upfates code

--- script.js
+++ script.js
@@ -1,71 +1,78 @@
+// // // // const person = {
+// // // //     name: 'selim',
+// // // //     fruit: 'dalim',
+// // // //     friends: ["alim", "kolim", "lamim"],
+// // // //     isRich: false,
+// // // //     money: 34000
+// // // // };
+
+// // // // console.log(person, typeof person)
+
+// // // // //JSON -> JS object with Notation
+// // // // //JSON.stringify
+// // // // //JSON.parse -> object
+
+// // // // const personJSON = JSON.stringify(person);
+// // // // console.log(personJSON, typeof personJSON);
+
+// // // // const parseJSON = JSON.parse(personJSON);
+// // // // console.log(parseJSON);
+
 // // // const person = {
-// // //     name: 'selim',
-// // //     fruit: 'dalim',
-// // //     friends: ["alim", "kolim", "lamim"],
-// // //     isRich: false,
-// // //     money: 34000
+// // //   name: 'selim',
+// // //   fruit: 'dalim',
+// // //   friends: ["alim", "kolim", "lamim"],
+// // //   isRich: false,
+// // //   money: 34000
 // // // };
-
-// // // console.log(person, typeof person)
-
-// // // //JSON -> JS object with Notation
-// // // //JSON.stringify
-// // // //JSON.parse -> object
+// // // console.log(person, typeof person);
 
 // // // const personJSON = JSON.stringify(person);
 // // // console.log(personJSON, typeof personJSON);
 
-// // // const parseJSON = JSON.parse(personJSON);
-// // // console.log(parseJSON);
 
-// // const person = {
-// //   name: 'selim',
-// //   fruit: 'dalim',
-// //   friends: ["alim", "kolim", "lamim"],
-// //   isRich: false,
-// //   money: 34000
+// // const task = {
+// //     id: 1,
+// //     title: "Learn JSON",
+// //     completed: false
 // // };
-// // console.log(person, typeof person);
 
-// // const personJSON = JSON.stringify(person);
-// // console.log(personJSON, typeof personJSON);
+// // const taskJSON = JSON.stringify(task);
+
+// // console.log(taskJSON);
+// // console.log(typeof taskJSON);
+
+// // const jsonString = '{"name":"Rafi","age":22,"skills":["JS","HTML"]}';
+
+// // const user = JSON.parse(jsonString);
+
+// // console.log(user);
+// // console.log(typeof user);
+
+// // console.log(user.name);
+// // console.log(user.skills[1]);
 
 
-// const task = {
-//     id: 1,
-//     title: "Learn JSON",
-//     completed: false
+// const product = {
+//     productName: "Laptop",
+//     price: 75000
 // };
 
-// const taskJSON = JSON.stringify(task);
+// const productJSON = JSON.stringify(product);
+// console.log(productJSON);
 
-// console.log(taskJSON);
-// console.log(typeof taskJSON);
-
-// const jsonString = '{"name":"Rafi","age":22,"skills":["JS","HTML"]}';
-
-// const user = JSON.parse(jsonString);
-
-// console.log(user);
-// console.log(typeof user);
-
-// console.log(user.name);
-// console.log(user.skills[1]);
+// const productobj = JSON.parse(productJSON);
+// console.log(productobj.productName);
+// console.log(productobj.price);
 
 
-const product = {
-    productName: "Laptop",
-    price: 75000
-};
-
-const productJSON = JSON.stringify(product);
-console.log(productJSON);
-
-const productobj = JSON.parse(productJSON);
-console.log(productobj.productName);
-console.log(productobj.price);
+// console.log(typeof product);      // object
+// console.log(typeof productJSON);  // string
+// console.log(typeof productObj);   // object
 
 
-console.log(typeof product);      // object
-console.log(typeof productJSON);  // string
-console.log(typeof productObj);   // object
+// const loadData = () => {
+//     fetch("https://jsonplaceholder.typicode.com/todos/1")
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+// }
